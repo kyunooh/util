2d20ef0769337b173d591d6438c86fb124b8d1b4
Hyeon-Mook Jerry Choi <chm073@gmail.com>
2016-05-07 19:36:22 +0900

    fix bugs

--- python/make_mysql_dummies.py
+++ python/make_mysql_dummies.py
@@ -14,29 +14,30 @@ connection = pymysql.connect(
 name_list = ['choi', 'hyeon', 'mook', 'kyun', 'jelly']
 favor_list = ['apple', 'banana', 'melon', 'lime', 'rasberry', 'coffee']
 
-try:
-    with connection.cursors as cs:
-        create_table_sql = """
-        CREATE TABLE IF NOT EXIST jelly_table (
-        jelly_id INT(10) PRIMARY KEY AUTO_INCREAMENT,
+
+with connection.cursor() as cs:
+    create_table_sql = """
+        CREATE TABLE IF NOT EXISTS jelly_table (
+        jelly_id INT(10) PRIMARY KEY AUTO_INCREMENT,
         jelly_name VARCHAR(150),
         jelly_favor VARCHAR(50),
         jelly_description TEXT
         )"""
-        cs.execute(create_table_sql)
-        print("creating table is done")
-        
-        for index in range(1, 10000):
-            insert_sql = """
-            INSERT INTO jelly_table (
+    cs.execute(create_table_sql)
+    print("creating table is done")
+    
+    for index in range(1, 10000):
+        insert_sql = """
+        INSERT INTO jelly_table (
             jelly_name,
             jelly_favor,
             jelly_description
             ) VALUES (%s, %s, %s)"""
-            
-            cs.execute(insert_sql, (
-                name_list[len(name_list) % index],
-                favor_list[len(favor_list) % index],
-                uuid.uuid4()))
-            print("index is : {}".format(index))
+        
+        cs.execute(insert_sql, (
+                name_list[index % len(name_list)],
+                favor_list[index % len(favor_list)],
+                str(uuid.uuid4())))
+        print("index is : {}".format(index))
 
+    
